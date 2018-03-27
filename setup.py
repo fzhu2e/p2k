b413e5f6628037b8a76d4fdf4138f52ad77655e5
Feng Zhu <lyricorpse@gmail.com>
2018-03-27 12:28:48 -0700

    Support pip install

--- setup.py
+++ setup.py
@@ -7,9 +7,6 @@ import os
 
 from setuptools import setup, find_packages
 
-if os.path.exists('MANIFEST'):
-    os.remove('MANIFEST')
-
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
@@ -30,14 +27,13 @@ test_requirements = [
 
 setup(
     name='p2k',
-    version='0.1.0',
+    version='0.3.0',
     description="A package to make life easier with PAGES2k dataset and stuff.",
     long_description=readme + '\n\n' + history,
     author="Feng Zhu",
     author_email='fengzhu@usc.edu',
     url='https://github.com/fzhu2e/p2k',
     packages=find_packages(),
-    package_data={'p2k': ['f2py_*.so']},
     include_package_data=True,
     install_requires=requirements,
     license="MIT license",
@@ -49,9 +45,8 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     test_suite='tests',
     tests_require=test_requirements,
